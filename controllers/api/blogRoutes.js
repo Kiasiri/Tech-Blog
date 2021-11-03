8ab2eb4cbbf0b2e46978ec7140ff84b31ac6e338
Matthew Wiessing <matthewwiessing@gmail.com>
2021-11-03 15:39:46 -0500

    prepared to attempt to deploy to heroku

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -4,3 +4,5 @@ const withAuth = require("../../utils/auth");
 //TODO: make a get all and get by id for comments and blogs
 //TODO: make an add blog and and add comment route
 //TODO: make an update and delete route for blogs and comments by id?
+
+module.exports = router;
--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,3 +1,18 @@
 const router = require("express").Router();
 const { User, Blog, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
+//TODO: make a get all and get by id for comments and blogs
+//TODO: make an add blog and and add comment route
+//TODO: make an update and delete route for blogs and comments by id?
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newCommentData = await Comment.create({
+      ...req.body,
+      comment_id: req.session.user_id,
+    });
+    res.status(200).json(newCommentData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+module.exports = router;
