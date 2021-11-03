c2b4617daa5f13ec74ebb388e23393f525881028
Matthew Wiessing <matthewwiessing@gmail.com>
2021-11-03 18:23:38 -0500

    made session last longer

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const hbs = exphbs.create({ helpers });
 // Configure and link a session object with the sequelize store
 const sess = {
   secret: "Super secret secret",
-  cookie: { maxAge: 10000 },
+  cookie: { maxAge: 100000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
