c22a02be94bbbb7387480fcef018226b4d904080
Matthew Wiessing <matthewwiessing@gmail.com>
2021-11-03 18:24:39 -0500

    copied additional helpers from activity 24

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,7 +4,12 @@ module.exports = {
   },
 };
 module.exports = {
+  format_time: (date) => {
+    return date.toLocaleTimeString();
+  },
   format_date: (date) => {
-    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
+      new Date(date).getFullYear() + 5
+    }`;
   },
 };
