d74bc1b102a762495b6137ce0cf1f9ad30109340
Matthew Wiessing <matthewwiessing@gmail.com>
2021-11-03 22:38:58 -0500

    added screenshot and made changes to post route still not working

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -6,7 +6,8 @@ router.post("/", withAuth, async (req, res) => {
   console.log(req.body);
   try {
     const newBlogData = await Blog.create({
-      ...req.body,
+      title: req.body.title,
+      text: req.body.text,
       user_id: req.session.user_id,
     });
     res.status(200).json(newBlogData);
